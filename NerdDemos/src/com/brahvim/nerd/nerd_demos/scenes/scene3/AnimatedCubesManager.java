b68357ca7b1efa6e579746410b1d0a996db90d86
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-02-28 23:37:20 +0530

    DOD can optimize stuff! + Decreased cube emission-rate.
    Tried existential processing for visible cubes, failed at it. Will do it later. Anyway, stuff's good for now!...

--- NerdDemos/src/com/brahvim/nerd/nerd_demos/scenes/scene3/AnimatedCubesManager.java
+++ NerdDemos/src/com/brahvim/nerd/nerd_demos/scenes/scene3/AnimatedCubesManager.java
@@ -33,38 +33,38 @@ public class AnimatedCubesManager {
 		protected static final float DEFAULT_DT_COEFFICIENT = 0.1f;
 		// endregion
 
-		protected final NerdSineEaseOld PLOP_WAVE;
 		protected final PVector
 		/*   */ POSITION = new PVector(),
 				VELOCITY = new PVector(),
 				ROTATION = new PVector(),
 				ACCELERATION = new PVector(),
-				ROTATION_VELOCITY = new PVector(),
+				ROTATIONAL_VELOCITY = new PVector(),
 				ROTATIONAL_ACCELERATION = new PVector();
 
 		protected int endTime;
-		protected boolean visible = AnimatedCube.DEFAULT_VISIBILITY;
+		protected boolean visible;
 		protected float
-		/*   */ size = AnimatedCube.DEFAULT_SIZE,
-				frict = AnimatedCube.DEFAULT_FRICTION,
-				dtCoef = AnimatedCube.DEFAULT_DT_COEFFICIENT,
-				rotFrict = AnimatedCube.DEFAULT_ROTATIONAL_FRICTION;
+		/*   */ size,
+				frict,
+				dtCoef,
+				rotFrict;
+		protected NerdSineEaseOld plopWave;
 
-		public AnimatedCube(final NerdSketch<?> p_sketch) {
-			this.PLOP_WAVE = new NerdSineEaseOld(p_sketch);
-			this.PLOP_WAVE.inactValue = 1;
-		}
+		// public AnimatedCube(final NerdSketch<?> p_sketch) {
+		// this.plopWave = new NerdSineEaseOld(p_sketch);
+		// this.plopWave.inactValue = 1;
+		// }
 
 	}
 
 	// region Fields.
 	public int
 	/*   */ cubesToAdd = 0,
-			cubesPerClick = 30,
-			cubesPerFrame = 12;
+			cubesPerClick = 3,
+			cubesPerFrame = 3;
 
-	private final Queue<AnimatedCube> FREE_CUBES = new ArrayDeque<>(3000);
-	private final List<AnimatedCube> CUBES = new ArrayList<>(3000);
+	private final Queue<AnimatedCube> FREE_CUBES = new ArrayDeque<>(6000);
+	private final List<AnimatedCube> CUBES = new ArrayList<>(6000);
 	private final NerdP3dSketch SKETCH;
 	private final NerdP3dScene SCENE;
 	private final PShape CUBE_SHAPE;
@@ -78,49 +78,44 @@ public class AnimatedCubesManager {
 
 	// region Cube methods.
 	private AnimatedCube createAnimatedCube() {
-		final AnimatedCube toRet;
+		final AnimatedCube toRet = this.FREE_CUBES.isEmpty()
+				? new AnimatedCube()
+				: this.FREE_CUBES.poll();
 
-		if (this.FREE_CUBES.isEmpty())
-			toRet = new AnimatedCube(this.SKETCH);
-		else {
-			toRet = this.FREE_CUBES.poll();
-			this.FREE_CUBES.remove(toRet);
-		}
-
+		toRet.plopWave = new NerdSineEaseOld(this.SKETCH);
+		toRet.plopWave.inactValue = 1;
+		// toRet.plopWave.resetValues();
 		this.CUBES.add(toRet);
 
-		final int cubeStartTime = this.SKETCH.millis();
-		toRet.endTime = cubeStartTime + AnimatedCube.DEFAULT_LIFETIME;
-
-		final PVector cameraPos = this.SCENE.GRAPHICS.getCurrentCamera().POSITION;
-
-		toRet.visible = true;
 		toRet.size = AnimatedCube.DEFAULT_SIZE;
 		toRet.frict = AnimatedCube.DEFAULT_FRICTION;
+		toRet.visible = AnimatedCube.DEFAULT_VISIBILITY;
 		toRet.dtCoef = AnimatedCube.DEFAULT_DT_COEFFICIENT;
 		toRet.rotFrict = AnimatedCube.DEFAULT_ROTATIONAL_FRICTION;
+		toRet.endTime = this.SKETCH.millis() + AnimatedCube.DEFAULT_LIFETIME;
 
-		toRet.POSITION.set(
-				cameraPos.x + this.SKETCH.random(-this.SKETCH.GENERIC_WINDOW.cx, this.SKETCH.GENERIC_WINDOW.cx),
-				cameraPos.y + this.SKETCH.random(-this.SKETCH.GENERIC_WINDOW.cy, this.SKETCH.GENERIC_WINDOW.cy),
-				cameraPos.z + this.SKETCH.random(-600, 600));
-
-		toRet.ACCELERATION.set(
-				this.SKETCH.random(-0.01f, 0.01f),
-				this.SKETCH.random(-0.01f, 0.01f),
-				this.SKETCH.random(-0.01f, 0.01f));
+		toRet.VELOCITY.set(0, 0, 0);
+		toRet.ROTATIONAL_VELOCITY.set(0, 0, 0);
+		toRet.POSITION.set(this.SCENE.GRAPHICS.getCurrentCamera().POSITION);
+		toRet.POSITION.add(
+				this.SKETCH.random(-this.SKETCH.GENERIC_WINDOW.cx, this.SKETCH.GENERIC_WINDOW.cx),
+				this.SKETCH.random(-this.SKETCH.GENERIC_WINDOW.cy, this.SKETCH.GENERIC_WINDOW.cy),
+				this.SKETCH.random(-600, 600));
 
 		toRet.ROTATION.set(
 				this.SKETCH.random(PConstants.TAU),
 				this.SKETCH.random(PConstants.TAU),
 				this.SKETCH.random(PConstants.TAU));
 
+		toRet.ACCELERATION.set(
+				this.SKETCH.random(-0.01f, 0.01f),
+				this.SKETCH.random(-0.01f, 0.01f),
+				this.SKETCH.random(-0.01f, 0.01f));
+
 		toRet.ROTATIONAL_ACCELERATION.set(
 				this.SKETCH.random(-0.0001f, 0.0001f),
 				this.SKETCH.random(-0.0001f, 0.0001f),
 				this.SKETCH.random(-0.0001f, 0.0001f));
-
-		toRet.PLOP_WAVE.resetSettings();
 
 		return toRet;
 	}
@@ -134,7 +129,7 @@ public class AnimatedCubesManager {
 		try (var a = g.new Push()) {
 			g.translate(p_cube.POSITION);
 			g.rotate(p_cube.ROTATION);
-			g.scale(p_cube.size * p_cube.PLOP_WAVE.get());
+			g.scale(p_cube.size * p_cube.plopWave.get());
 
 			g.strokeWeight(0.018f);
 			g.fill(255);
@@ -150,22 +145,25 @@ public class AnimatedCubesManager {
 		final float deltaTime = this.SKETCH.getFrameTime() * p_cube.dtCoef;
 
 		// Physics calculations:
+
+		p_cube.ROTATIONAL_VELOCITY.add(p_cube.ROTATIONAL_ACCELERATION);
+		p_cube.ROTATION.add(PVector.mult(p_cube.ROTATIONAL_VELOCITY, p_cube.rotFrict + deltaTime));
+
 		p_cube.VELOCITY.add(p_cube.ACCELERATION);
-		p_cube.ROTATION_VELOCITY.add(p_cube.ROTATIONAL_ACCELERATION);
 		p_cube.POSITION.add(PVector.mult(p_cube.VELOCITY, p_cube.frict + deltaTime));
-		p_cube.ROTATION.add(PVector.mult(p_cube.ROTATION_VELOCITY, p_cube.rotFrict + deltaTime));
+
 	}
 
 	protected AnimatedCube beginCubePlopIn(final AnimatedCube p_cube) {
 		p_cube.visible = true;
-		p_cube.PLOP_WAVE.parameterCoef = 0.015f;
-		p_cube.PLOP_WAVE.endWhenAngleIncrementsBy(PConstants.HALF_PI).start();
+		p_cube.plopWave.parameterCoef = 0.015f;
+		p_cube.plopWave.endWhenAngleIncrementsBy(PConstants.HALF_PI).start();
 		return p_cube;
 	}
 
 	protected AnimatedCube beginCubePlopOut(final AnimatedCube p_cube) {
-		p_cube.PLOP_WAVE.parameterCoef = 0.00001f;
-		p_cube.PLOP_WAVE.endWhenAngleIncrementsBy(PConstants.HALF_PI)
+		p_cube.plopWave.parameterCoef = 0.00001f;
+		p_cube.plopWave.endWhenAngleIncrementsBy(PConstants.HALF_PI)
 				.start(270, () -> p_cube.visible = false);
 
 		return p_cube;
@@ -185,25 +183,21 @@ public class AnimatedCubesManager {
 			if (cube.endTime - millis < 0)
 				this.beginCubePlopOut(cube);
 
-			// If it actually isn't visible anymore, remove it:
-			// if (!cube.visible) {
-			// it.remove();
-			// this.FREE_CUBES.add(cube);
-			// }
-
 			// Draw the cube!:
 			this.updateCube(cube);
 			this.drawCube(cube);
 		}
 
-		for (int i = this.CUBES.size(); i > -1; --i) {
+		// Removing these later so we're still friends with the cache!
+		// It might also be faster!
+		for (int i = this.CUBES.size() - 1; i > -1; --i) {
 			final AnimatedCube cube = this.CUBES.get(i);
 
 			if (cube.visible)
 				continue;
 
+			this.FREE_CUBES.offer(cube);
 			this.CUBES.remove(i);
-			this.FREE_CUBES.add(cube);
 		}
 	}
 
@@ -212,7 +206,7 @@ public class AnimatedCubesManager {
 			return;
 
 		for (int i = 0; //
-				i <= this.cubesPerFrame || this.cubesToAdd == 0; //
+				i < this.cubesPerFrame || this.cubesToAdd == 0; //
 				++i, --this.cubesToAdd) // Yes, two things at once! Not just a C++ feature!
 			this.beginCubePlopIn(this.createAnimatedCube());
 
