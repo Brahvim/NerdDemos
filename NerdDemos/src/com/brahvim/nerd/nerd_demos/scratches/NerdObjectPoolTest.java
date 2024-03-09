6897391ce72d4f28a6e359a5430bfedaf5021550
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-03-09 23:34:25 +0530

    Runs well so far.

--- NerdDemos/src/com/brahvim/nerd/nerd_demos/scratches/NerdObjectPoolTest.java
+++ NerdDemos/src/com/brahvim/nerd/nerd_demos/scratches/NerdObjectPoolTest.java
@@ -7,42 +7,43 @@ import com.brahvim.nerd.utils.NerdObjectPool;
 public class NerdObjectPoolTest {
 
     public static void main(final String[] args) {
-        // Create an object pool of integers
+        // Create an object pool of `float`s:
         final NerdObjectPool<Float> floatsPool = new NerdObjectPool<>(5);
 
-        // Promote some objects to the pool
+        // Promote some objects in:
         for (int i = 0; i < 10; i++) {
             floatsPool.promote((float) i);
         }
 
-        // Display the demoted objects
+        // Display the demoted stuff:
         System.out.println("Demoted Objects:");
         for (final Float demoted : floatsPool.allDemoted()) {
             System.out.print(demoted + " ");
         }
         System.out.println();
 
-        // Demote some objects back to the pool
+        // Demote some objects back to the pool:
         final Iterator<Float> promotedIterator = floatsPool.promotedObjectsIterator();
         while (promotedIterator.hasNext()) {
             final Float demoted = promotedIterator.next();
             if (demoted % 2 == 0) {
-                promotedIterator.remove(); // TODO: Implement this to demote objects!
+                promotedIterator.remove();
             }
         }
 
-        // Display the demoted objects after demotion
+        // Display the demoted objects after demotion using the other iterator:
+        final Iterator<Float> demotedIterator = floatsPool.demotedObjectsIterator();
         System.out.println("Demoted Objects after Demotion:");
-        for (final Float demoted : floatsPool.allDemoted()) {
-            System.out.print(demoted + " ");
+        while (demotedIterator.hasNext()) {
+            System.out.print(demotedIterator.next() + " ");
         }
         System.out.println();
 
-        // Promote some objects again
+        // Promote some objects again:
         floatsPool.promote(3);
         floatsPool.promote(7);
 
-        // Display the promoted objects
+        // Display the promoted objects!:
         System.out.println("Promoted Objects:");
         for (final Float promoted : floatsPool.allPromoted()) {
             System.out.print(promoted + " ");
